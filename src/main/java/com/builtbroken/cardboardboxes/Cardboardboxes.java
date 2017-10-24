8969a0af7855f8f47c7334ea82889956c326591d
Robert WC Seifert <rseifert.phone@gmail.com>
2017-10-24 10:38:14 -0400

    Reworked configs to solve server side issues and improve usability

--- src/main/java/com/builtbroken/cardboardboxes/Cardboardboxes.java
+++ src/main/java/com/builtbroken/cardboardboxes/Cardboardboxes.java
@@ -5,10 +5,10 @@ import com.builtbroken.cardboardboxes.box.ItemBlockBox;
 import com.builtbroken.cardboardboxes.box.TileBox;
 import com.builtbroken.cardboardboxes.handler.HandlerManager;
 import com.builtbroken.cardboardboxes.mods.IC2Handler;
-import com.builtbroken.cardboardboxes.mods.buildcraft.BuildCraftEnergyHandler;
-import com.builtbroken.cardboardboxes.mods.buildcraft.BuildCraftFactoryHandler;
 import com.builtbroken.cardboardboxes.mods.ModSupportHandler;
 import com.builtbroken.cardboardboxes.mods.TinkersConstructHandler;
+import com.builtbroken.cardboardboxes.mods.buildcraft.BuildCraftEnergyHandler;
+import com.builtbroken.cardboardboxes.mods.buildcraft.BuildCraftFactoryHandler;
 import com.builtbroken.cardboardboxes.mods.buildcraft.BuildCraftTransportHandler;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
@@ -38,6 +38,8 @@ public class Cardboardboxes
     public static final String DOMAIN = "cardboardboxes";
     public static final String PREFIX = DOMAIN + ":";
 
+    public static final String blackListConfigCategory = "BlackListTilesByID";
+
     @SidedProxy(clientSide = "com.builtbroken.cardboardboxes.ClientProxy", serverSide = "com.builtbroken.cardboardboxes.CommonProxy")
     public static CommonProxy proxy;
 
@@ -49,6 +51,7 @@ public class Cardboardboxes
     public static HandlerManager boxHandler;
 
     public static HashMap<String, Class<? extends ModSupportHandler>> modSupportHandlerMap = new HashMap();
+
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
@@ -125,30 +128,40 @@ public class Cardboardboxes
         modSupportHandlerMap.put("BuildCraft|Builders", BuildCraftTransportHandler.class);
         modSupportHandlerMap.put("IC2", IC2Handler.class);
         modSupportHandlerMap.put("appliedenergistics2", ModSupportHandler.class);
+
+        //Load tiles
         try
         {
+            //Get tile map field
             Field field;
             try
             {
                 field = TileEntity.class.getDeclaredField("field_145855_i");
-            } catch (NoSuchFieldException e)
+            }
+            catch (NoSuchFieldException e)
             {
                 field = TileEntity.class.getDeclaredField("nameToClassMap");
             }
             field.setAccessible(true);
+
+            //Loop over tiles
             Map<String, Class> map = (Map) field.get(null);
+
+            //Load mod support and check for blacklist
             for (Map.Entry<String, Class<? extends ModSupportHandler>> entry : modSupportHandlerMap.entrySet())
             {
                 if (Loader.isModLoaded(entry.getKey()))
                 {
                     try
                     {
-                        entry.getValue().newInstance().handleBlackListedContent(map);
-                    } catch (InstantiationException e)
+                        entry.getValue().newInstance().handleBlackListedContent(map); //TODO rework
+                    }
+                    catch (InstantiationException e)
                     {
                         LOGGER.error("Failed to create handler for mod " + entry.getKey());
                         e.printStackTrace();
-                    } catch (IllegalAccessException e)
+                    }
+                    catch (IllegalAccessException e)
                     {
                         LOGGER.error("Failed to access constructor for handler for mod " + entry.getKey());
                         e.printStackTrace();
@@ -156,47 +169,64 @@ public class Cardboardboxes
                 }
             }
 
+
             //TODO see if we can sort the files by mod to help users find what they are looking for
+
+            //Loop over tile entities adding them to the config for disabling
             for (Map.Entry<String, Class> entry : map.entrySet())
             {
                 try
                 {
-                    config.setCategoryComment("BlackListTilesByName", "Auto generated list of tiles registered in Minecraft that can be blacklisted. If a tile does not show up on this list it is already black listed. The reasoning behind blacklisting tiles is to prevent crashes or unwanted interaction. Such as picking up a piston which can both causes issues and doesn't really matter.");
-                    Class<? extends TileEntity> clazz = entry.getValue();
-                    String name = entry.getKey();
-                    if (name != null && !name.isEmpty())
+                    config.setCategoryComment(blackListConfigCategory, "Auto generated list of tiles registered in Minecraft that can be blacklisted. " +
+                            "If a tile does not show up on this list it is already black listed. The reasoning behind blacklisting tiles is to prevent " +
+                            "crashes or unwanted interaction. Such as picking up a piston which can both causes issues and doesn't really matter.");
+
+                    //Get tile id
+                    final String registeredID = entry.getKey();
+                    if (registeredID != null && !registeredID.isEmpty())
                     {
-                        if (clazz != null)
+                        //Get tile class
+                        final Class<? extends TileEntity> tileClazz = entry.getValue();
+                        if (tileClazz != null)
                         {
-                            String clazzName = clazz.getSimpleName();
-                            boolean shouldBan = boxHandler.blackListedTiles.contains(clazz) || clazzName.contains("cable") || clazzName.contains("wire") || clazzName.contains("pipe") || clazzName.contains("tube") || clazzName.contains("conduit") || clazzName.contains("channel");
-                            if (config.getBoolean("" + clazz, "BlackListTilesByName", shouldBan, "Prevents the cardboard box from picking up this tile[" + name + "]"))
+                            //Get class as a string
+                            final String clazzName = tileClazz.getName().replace("class ", "");
+
+                            //Check if tile should be banned by default
+                            boolean shouldBan = boxHandler.blackListedTiles.contains(tileClazz) || clazzName.contains("cable") || clazzName.contains("wire") || clazzName.contains("pipe") || clazzName.contains("tube") || clazzName.contains("conduit") || clazzName.contains("channel");
+
+                            //Check config
+                            if (config.getBoolean("" + registeredID, blackListConfigCategory, shouldBan, "Prevents the cardboard box from picking up this tile[" + clazzName + "]"))
                             {
-                                boxHandler.banTile(clazz);
+                                boxHandler.banTile(tileClazz);
                             }
-                            else if (shouldBan && boxHandler.blackListedTiles.contains(clazz))
+                            //Check blacklist
+                            else if (shouldBan && boxHandler.blackListedTiles.contains(tileClazz))
                             {
                                 //If original was banned but someone unbanned it in the config
-                                boxHandler.blackListedTiles.remove(clazz);
+                                boxHandler.blackListedTiles.remove(tileClazz);
                             }
                         }
                     }
-                } catch (Exception e)
+                }
+                catch (Exception e)
                 {
                     LOGGER.error("Failed to add entry to config " + entry);
                     e.printStackTrace();
                 }
-
             }
-        } catch (NoSuchFieldException e)
+        }
+        catch (NoSuchFieldException e)
         {
             LOGGER.error("Failed to find the tile map field");
             e.printStackTrace();
-        } catch (IllegalAccessException e)
+        }
+        catch (IllegalAccessException e)
         {
             LOGGER.error("Failed to access tile map");
             e.printStackTrace();
-        } catch (Exception e)
+        }
+        catch (Exception e)
         {
             LOGGER.error("Failed to add tile map to config");
             e.printStackTrace();
