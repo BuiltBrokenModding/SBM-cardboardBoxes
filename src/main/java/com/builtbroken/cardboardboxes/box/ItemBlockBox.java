8a8e36c9c02a1b802eefe740e4589c3d490daa8d
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-01-12 00:36:27 +0100

    fix whitespace

--- src/main/java/com/builtbroken/cardboardboxes/box/ItemBlockBox.java
+++ src/main/java/com/builtbroken/cardboardboxes/box/ItemBlockBox.java
@@ -39,234 +39,234 @@ import net.minecraft.world.World;
  */
 public class ItemBlockBox extends ItemBlock
 {
-	public ItemBlockBox(Block block)
-	{
-		super(block);
-		this.setRegistryName(block.getRegistryName());
-		this.setHasSubtypes(true);
-	}
+    public ItemBlockBox(Block block)
+    {
+        super(block);
+        this.setRegistryName(block.getRegistryName());
+        this.setHasSubtypes(true);
+    }
 
-	//TODO add property to change render if contains item
-	//TODO add property to change render based on content (e.g. show chest on box)
-	//TODO add property to change render color, label, etc
+    //TODO add property to change render if contains item
+    //TODO add property to change render based on content (e.g. show chest on box)
+    //TODO add property to change render color, label, etc
 
-	@Override
-	public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
-	{
-		//Run all logic server side
-		if (worldIn.isRemote)
-		{
-			return EnumActionResult.SUCCESS;
-		}
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        //Run all logic server side
+        if (worldIn.isRemote)
+        {
+            return EnumActionResult.SUCCESS;
+        }
 
-		final ItemStack heldItemStack = player.getHeldItem(hand);
-		if (!heldItemStack.isEmpty())
-		{
-			final ItemStack storedStack = getStoredBlock(heldItemStack);
-			if (!storedStack.isEmpty())
-			{
-				return tryToPlaceBlock(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
-			}
-			else
-			{
-				return tryToPickupBlock(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
-			}
-		}
-		return EnumActionResult.FAIL;
-	}
+        final ItemStack heldItemStack = player.getHeldItem(hand);
+        if (!heldItemStack.isEmpty())
+        {
+            final ItemStack storedStack = getStoredBlock(heldItemStack);
+            if (!storedStack.isEmpty())
+            {
+                return tryToPlaceBlock(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+            }
+            else
+            {
+                return tryToPickupBlock(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+            }
+        }
+        return EnumActionResult.FAIL;
+    }
 
-	protected EnumActionResult tryToPickupBlock(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
-	{
-		//Check that we can pick up block
-		CanPickUpResult result = HandlerManager.INSTANCE.canPickUp(worldIn, pos);
-		if (result == CanPickUpResult.CAN_PICK_UP)
-		{
-			//Get tile, ignore anything without a tile
-			TileEntity tileEntity = worldIn.getTileEntity(pos);
-			if (tileEntity != null)
-			{
-				//Get stack
-				final IBlockState state = worldIn.getBlockState(pos);
-				final ItemStack blockStack = new ItemStack(Item.getItemFromBlock(state.getBlock()));
-				if (!blockStack.isEmpty())
-				{
-					//Copy tile data
-					NBTTagCompound nbtTagCompound = new NBTTagCompound();
-					tileEntity.writeToNBT(nbtTagCompound);
+    protected EnumActionResult tryToPickupBlock(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        //Check that we can pick up block
+        CanPickUpResult result = HandlerManager.INSTANCE.canPickUp(worldIn, pos);
+        if (result == CanPickUpResult.CAN_PICK_UP)
+        {
+            //Get tile, ignore anything without a tile
+            TileEntity tileEntity = worldIn.getTileEntity(pos);
+            if (tileEntity != null)
+            {
+                //Get stack
+                final IBlockState state = worldIn.getBlockState(pos);
+                final ItemStack blockStack = new ItemStack(Item.getItemFromBlock(state.getBlock()));
+                if (!blockStack.isEmpty())
+                {
+                    //Copy tile data
+                    NBTTagCompound nbtTagCompound = new NBTTagCompound();
+                    tileEntity.writeToNBT(nbtTagCompound);
 
-					//Remove location data
-					nbtTagCompound.removeTag("x");
-					nbtTagCompound.removeTag("y");
-					nbtTagCompound.removeTag("z");
+                    //Remove location data
+                    nbtTagCompound.removeTag("x");
+                    nbtTagCompound.removeTag("y");
+                    nbtTagCompound.removeTag("z");
 
-					//Remove tile
-					worldIn.removeTileEntity(pos);
+                    //Remove tile
+                    worldIn.removeTileEntity(pos);
 
-					//Replace block with our block
-					worldIn.setBlockState(pos, Cardboardboxes.blockBox.getDefaultState(), 2);
+                    //Replace block with our block
+                    worldIn.setBlockState(pos, Cardboardboxes.blockBox.getDefaultState(), 2);
 
-					//Get our tile
-					tileEntity = worldIn.getTileEntity(pos);
-					if (tileEntity instanceof TileEntityBox)
-					{
-						TileEntityBox tileBox = (TileEntityBox) tileEntity;
+                    //Get our tile
+                    tileEntity = worldIn.getTileEntity(pos);
+                    if (tileEntity instanceof TileEntityBox)
+                    {
+                        TileEntityBox tileBox = (TileEntityBox) tileEntity;
 
-						//Move data into tile
-						tileBox.setItemForPlacement(blockStack);
-						tileBox.setDataForPlacement(nbtTagCompound);
+                        //Move data into tile
+                        tileBox.setItemForPlacement(blockStack);
+                        tileBox.setDataForPlacement(nbtTagCompound);
 
-						//Consume item
-						player.getHeldItem(hand).shrink(1);
+                        //Consume item
+                        player.getHeldItem(hand).shrink(1);
 
-						//Done
-						return EnumActionResult.SUCCESS;
-					}
-				}
-				else
-				{
-					player.sendStatusMessage(new TextComponentTranslation(getTranslationKey() + ".noItem.name"), true);
-				}
-			}
-			else
-			{
-				player.sendStatusMessage(new TextComponentTranslation(getTranslationKey() + ".noData.name"), true);
-			}
-		}
-		else if (result == CanPickUpResult.BANNED_TILE)
-		{
-			player.sendStatusMessage(new TextComponentTranslation(getTranslationKey() + ".banned.tile.name"), true);
-		}
-		else if (result == CanPickUpResult.BANNED_BLOCK)
-		{
-			player.sendStatusMessage(new TextComponentTranslation(getTranslationKey() + ".banned.block.name"), true);
-		}
-		else
-		{
-			player.sendStatusMessage(new TextComponentTranslation(getTranslationKey() + ".noData.name"), true);
-		}
-		return EnumActionResult.SUCCESS;
-	}
+                        //Done
+                        return EnumActionResult.SUCCESS;
+                    }
+                }
+                else
+                {
+                    player.sendStatusMessage(new TextComponentTranslation(getTranslationKey() + ".noItem.name"), true);
+                }
+            }
+            else
+            {
+                player.sendStatusMessage(new TextComponentTranslation(getTranslationKey() + ".noData.name"), true);
+            }
+        }
+        else if (result == CanPickUpResult.BANNED_TILE)
+        {
+            player.sendStatusMessage(new TextComponentTranslation(getTranslationKey() + ".banned.tile.name"), true);
+        }
+        else if (result == CanPickUpResult.BANNED_BLOCK)
+        {
+            player.sendStatusMessage(new TextComponentTranslation(getTranslationKey() + ".banned.block.name"), true);
+        }
+        else
+        {
+            player.sendStatusMessage(new TextComponentTranslation(getTranslationKey() + ".noData.name"), true);
+        }
+        return EnumActionResult.SUCCESS;
+    }
 
-	protected EnumActionResult tryToPlaceBlock(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
-	{
-		IBlockState iblockstate = worldIn.getBlockState(pos);
-		Block block = iblockstate.getBlock();
+    protected EnumActionResult tryToPlaceBlock(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        IBlockState iblockstate = worldIn.getBlockState(pos);
+        Block block = iblockstate.getBlock();
 
-		//Move up one if not replaceable
-		if (!block.isReplaceable(worldIn, pos))
-		{
-			pos = pos.offset(facing);
-		}
+        //Move up one if not replaceable
+        if (!block.isReplaceable(worldIn, pos))
+        {
+            pos = pos.offset(facing);
+        }
 
-		final ItemStack heldItemStack = player.getHeldItem(hand);
-		final ItemStack storeBlockAsItemStack = getStoredBlock(heldItemStack);
-		final Block storedBlock = Block.getBlockFromItem(storeBlockAsItemStack.getItem());
-		final NBTTagCompound savedTileData = getStoredTileData(heldItemStack);
+        final ItemStack heldItemStack = player.getHeldItem(hand);
+        final ItemStack storeBlockAsItemStack = getStoredBlock(heldItemStack);
+        final Block storedBlock = Block.getBlockFromItem(storeBlockAsItemStack.getItem());
+        final NBTTagCompound savedTileData = getStoredTileData(heldItemStack);
 
-		//Check if we can place the given block
-		if (storedBlock != null && player.canPlayerEdit(pos, facing, heldItemStack) && worldIn.mayPlace(storedBlock, pos, false, facing, (Entity) null))
-		{
-			Handler handler = HandlerManager.INSTANCE.getHandler(storedBlock);
+        //Check if we can place the given block
+        if (storedBlock != null && player.canPlayerEdit(pos, facing, heldItemStack) && worldIn.mayPlace(storedBlock, pos, false, facing, (Entity) null))
+        {
+            Handler handler = HandlerManager.INSTANCE.getHandler(storedBlock);
 
-			int meta = this.getMetadata(heldItemStack.getMetadata());
-			IBlockState blockstate = storedBlock.getStateForPlacement(worldIn, pos, facing, hitX, hitY, hitZ, meta, player, hand);
+            int meta = this.getMetadata(heldItemStack.getMetadata());
+            IBlockState blockstate = storedBlock.getStateForPlacement(worldIn, pos, facing, hitX, hitY, hitZ, meta, player, hand);
 
-			//Allow handler to control placement
-			if (handler != null && handler.placeBlock(player, worldIn, pos, hand, facing, hitX, hitY, hitZ, storeBlockAsItemStack, savedTileData)
-					//Run normal placement if we don't have a handler or it didn't do anything
-					|| placeBlockAt(heldItemStack, player, worldIn, pos, facing, hitX, hitY, hitZ, blockstate))
-			{
-				//Get placed block
-				blockstate = worldIn.getBlockState(pos);
+            //Allow handler to control placement
+            if (handler != null && handler.placeBlock(player, worldIn, pos, hand, facing, hitX, hitY, hitZ, storeBlockAsItemStack, savedTileData)
+                    //Run normal placement if we don't have a handler or it didn't do anything
+                    || placeBlockAt(heldItemStack, player, worldIn, pos, facing, hitX, hitY, hitZ, blockstate))
+            {
+                //Get placed block
+                blockstate = worldIn.getBlockState(pos);
 
-				//Allow handle to do post placement modification (e.g. fix rotation)
-				if (handler != null)
-				{
-					handler.postPlaceBlock(player, worldIn, pos, hand, facing, hitX, hitY, hitZ, storeBlockAsItemStack, savedTileData);
-				}
+                //Allow handle to do post placement modification (e.g. fix rotation)
+                if (handler != null)
+                {
+                    handler.postPlaceBlock(player, worldIn, pos, hand, facing, hitX, hitY, hitZ, storeBlockAsItemStack, savedTileData);
+                }
 
-				//Handle TileEntity
-				final TileEntity tileEntity = worldIn.getTileEntity(pos);
-				if (tileEntity != null)
-				{
-					//Set tile entity data
-					if (savedTileData != null)
-					{
-						//Load data
-						if (handler != null)
-						{
-							handler.loadData(tileEntity, savedTileData);
-						}
-						else
-						{
-							tileEntity.readFromNBT(savedTileData);
-						}
+                //Handle TileEntity
+                final TileEntity tileEntity = worldIn.getTileEntity(pos);
+                if (tileEntity != null)
+                {
+                    //Set tile entity data
+                    if (savedTileData != null)
+                    {
+                        //Load data
+                        if (handler != null)
+                        {
+                            handler.loadData(tileEntity, savedTileData);
+                        }
+                        else
+                        {
+                            tileEntity.readFromNBT(savedTileData);
+                        }
 
-						//Force position
-						tileEntity.setPos(pos);
-					}
+                        //Force position
+                        tileEntity.setPos(pos);
+                    }
 
-					//Allow handle to do post placement modification (e.g. fix rotation)
-					if (handler != null)
-					{
-						handler.postPlaceTileEntity(player, tileEntity, hand, facing, hitX, hitY, hitZ, storeBlockAsItemStack, savedTileData);
-					}
-				}
+                    //Allow handle to do post placement modification (e.g. fix rotation)
+                    if (handler != null)
+                    {
+                        handler.postPlaceTileEntity(player, tileEntity, hand, facing, hitX, hitY, hitZ, storeBlockAsItemStack, savedTileData);
+                    }
+                }
 
 
-				//Place audio
-				SoundType soundtype = blockstate.getBlock().getSoundType(blockstate, worldIn, pos, player);
-				worldIn.playSound(null, pos, soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
+                //Place audio
+                SoundType soundtype = blockstate.getBlock().getSoundType(blockstate, worldIn, pos, player);
+                worldIn.playSound(null, pos, soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
 
-				//Consume item
-				heldItemStack.shrink(1);
+                //Consume item
+                heldItemStack.shrink(1);
 
-				//Return empty box
-				if (!player.isCreative() && !player.inventory.addItemStackToInventory(new ItemStack(Cardboardboxes.blockBox)))
-				{
-					player.entityDropItem(new ItemStack(Cardboardboxes.blockBox), 0F);
-				}
-			}
+                //Return empty box
+                if (!player.isCreative() && !player.inventory.addItemStackToInventory(new ItemStack(Cardboardboxes.blockBox)))
+                {
+                    player.entityDropItem(new ItemStack(Cardboardboxes.blockBox), 0F);
+                }
+            }
 
-			return EnumActionResult.SUCCESS;
-		}
-		else
-		{
-			return EnumActionResult.FAIL;
-		}
-	}
+            return EnumActionResult.SUCCESS;
+        }
+        else
+        {
+            return EnumActionResult.FAIL;
+        }
+    }
 
-	@Override
-	public int getItemStackLimit(ItemStack stack)
-	{
-		return stack.hasTagCompound() ? 1 : 64;
-	}
+    @Override
+    public int getItemStackLimit(ItemStack stack)
+    {
+        return stack.hasTagCompound() ? 1 : 64;
+    }
 
-	@Override
-	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
-	{
-		if (stack.getTagCompound() != null && stack.getTagCompound().hasKey(STORE_ITEM_TAG))
-		{
-			ItemStack storedStack = new ItemStack(stack.getTagCompound().getCompoundTag(STORE_ITEM_TAG));
-			String name = storedStack.getDisplayName();
-			if (name != null && !name.isEmpty())
-			{
-				tooltip.add(name);
-			}
-			else
-			{
-				tooltip.add("" + storedStack);
-			}
-		}
-	}
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+        if (stack.getTagCompound() != null && stack.getTagCompound().hasKey(STORE_ITEM_TAG))
+        {
+            ItemStack storedStack = new ItemStack(stack.getTagCompound().getCompoundTag(STORE_ITEM_TAG));
+            String name = storedStack.getDisplayName();
+            if (name != null && !name.isEmpty())
+            {
+                tooltip.add(name);
+            }
+            else
+            {
+                tooltip.add("" + storedStack);
+            }
+        }
+    }
 
-	public ItemStack getStoredBlock(ItemStack stack)
-	{
-		return stack.getTagCompound() != null && stack.getTagCompound().hasKey(STORE_ITEM_TAG) ? new ItemStack(stack.getTagCompound().getCompoundTag(STORE_ITEM_TAG)) : ItemStack.EMPTY;
-	}
+    public ItemStack getStoredBlock(ItemStack stack)
+    {
+        return stack.getTagCompound() != null && stack.getTagCompound().hasKey(STORE_ITEM_TAG) ? new ItemStack(stack.getTagCompound().getCompoundTag(STORE_ITEM_TAG)) : ItemStack.EMPTY;
+    }
 
-	public NBTTagCompound getStoredTileData(ItemStack stack)
-	{
-		return stack.getTagCompound() != null && stack.getTagCompound().hasKey(TILE_DATA_TAG) ? stack.getTagCompound().getCompoundTag(TILE_DATA_TAG) : null;
-	}
+    public NBTTagCompound getStoredTileData(ItemStack stack)
+    {
+        return stack.getTagCompound() != null && stack.getTagCompound().hasKey(TILE_DATA_TAG) ? stack.getTagCompound().getCompoundTag(TILE_DATA_TAG) : null;
+    }
 }
--- src/main/java/com/builtbroken/cardboardboxes/handler/HandlerManager.java
+++ src/main/java/com/builtbroken/cardboardboxes/handler/HandlerManager.java
@@ -18,106 +18,106 @@ import net.minecraft.world.World;
  */
 public class HandlerManager
 {
-	/** Map of tiles to handlers that provide special interaction */
-	public static HashMap<Class<? extends TileEntity>, Handler> pickupHandlerMap = new HashMap<>();
+    /** Map of tiles to handlers that provide special interaction */
+    public static HashMap<Class<? extends TileEntity>, Handler> pickupHandlerMap = new HashMap<>();
 
-	/** Map of block to handlers that provide special interaction */
-	public static HashMap<Block, Handler> handlerMap = new HashMap<>();
+    /** Map of block to handlers that provide special interaction */
+    public static HashMap<Block, Handler> handlerMap = new HashMap<>();
 
-	/** List of tiles that are banned */
-	public static List<Class<? extends TileEntity>> tileEntityBanList = new ArrayList<>();
+    /** List of tiles that are banned */
+    public static List<Class<? extends TileEntity>> tileEntityBanList = new ArrayList<>();
 
-	/** List of Blocks that are banned */
-	public static List<Block> blockBanList = new ArrayList<>();
+    /** List of Blocks that are banned */
+    public static List<Block> blockBanList = new ArrayList<>();
 
-	/** Primary manager */
-	public final static HandlerManager INSTANCE = new HandlerManager();
+    /** Primary manager */
+    public final static HandlerManager INSTANCE = new HandlerManager();
 
 
-	/**
-	 * Called to register a handler for managing the pickup state of a tile
-	 *
-	 * @param clazz
-	 * @param handler
-	 */
-	public void registerPickupHandler(Class<? extends TileEntity> clazz, Handler handler) //TODO implement
-	{
-		pickupHandlerMap.put(clazz, handler);
-	}
+    /**
+     * Called to register a handler for managing the pickup state of a tile
+     *
+     * @param clazz
+     * @param handler
+     */
+    public void registerPickupHandler(Class<? extends TileEntity> clazz, Handler handler) //TODO implement
+    {
+        pickupHandlerMap.put(clazz, handler);
+    }
 
-	/**
-	 * Called to register an interaction handler to manage the overall state of a block
-	 *
-	 * @param block   - block to handle
-	 * @param handler - object to manage handler calls
-	 */
-	public void registerHandler(Block block, Handler handler)
-	{
-		handlerMap.put(block, handler);
-	}
+    /**
+     * Called to register an interaction handler to manage the overall state of a block
+     *
+     * @param block   - block to handle
+     * @param handler - object to manage handler calls
+     */
+    public void registerHandler(Block block, Handler handler)
+    {
+        handlerMap.put(block, handler);
+    }
 
-	public Handler getHandler(Block block)
-	{
-		return handlerMap.get(block);
-	}
+    public Handler getHandler(Block block)
+    {
+        return handlerMap.get(block);
+    }
 
-	/**
-	 * Called to ban a tile
-	 *
-	 * @param clazz
-	 */
-	public void banTile(Class<? extends TileEntity> clazz)
-	{
-		if (!tileEntityBanList.contains(clazz))
-		{
-			tileEntityBanList.add(clazz);
-		}
-	}
+    /**
+     * Called to ban a tile
+     *
+     * @param clazz
+     */
+    public void banTile(Class<? extends TileEntity> clazz)
+    {
+        if (!tileEntityBanList.contains(clazz))
+        {
+            tileEntityBanList.add(clazz);
+        }
+    }
 
-	/**
-	 * Called to ban a block
-	 *
-	 * @param block
-	 */
-	public void banBlock(Block block)
-	{
-		if (!blockBanList.contains(block))
-		{
-			blockBanList.add(block);
-		}
-	}
+    /**
+     * Called to ban a block
+     *
+     * @param block
+     */
+    public void banBlock(Block block)
+    {
+        if (!blockBanList.contains(block))
+        {
+            blockBanList.add(block);
+        }
+    }
 
-	/**
-	 * Called to check if a block can be picked up inside a box
-	 *
-	 * @param world - position
-	 * @param pos   - position
-	 * @return result of the interaction
-	 */
-	public CanPickUpResult canPickUp(World world, BlockPos pos)
-	{
-		Block block = world.getBlockState(pos).getBlock();
-		if (!blockBanList.contains(block))
-		{
-			TileEntity tile = world.getTileEntity(pos);
-			if (tile != null)
-			{
-				if (!tileEntityBanList.contains(tile.getClass()))
-				{
-					//Check if we even have data to store, no data no point in using a box
-					NBTTagCompound nbt = new NBTTagCompound();
-					tile.writeToNBT(nbt);
-					nbt.removeTag("x");
-					nbt.removeTag("y");
-					nbt.removeTag("z");
-					nbt.removeTag("id");
-					return !nbt.isEmpty() ? CanPickUpResult.CAN_PICK_UP : CanPickUpResult.NO_DATA;
-				}
-				return CanPickUpResult.BANNED_TILE;
-			}
-			return CanPickUpResult.NO_TILE;
-		}
-		return CanPickUpResult.BANNED_BLOCK;
-	}
+    /**
+     * Called to check if a block can be picked up inside a box
+     *
+     * @param world - position
+     * @param pos   - position
+     * @return result of the interaction
+     */
+    public CanPickUpResult canPickUp(World world, BlockPos pos)
+    {
+        Block block = world.getBlockState(pos).getBlock();
+        if (!blockBanList.contains(block))
+        {
+            TileEntity tile = world.getTileEntity(pos);
+            if (tile != null)
+            {
+                if (!tileEntityBanList.contains(tile.getClass()))
+                {
+                    //Check if we even have data to store, no data no point in using a box
+                    NBTTagCompound nbt = new NBTTagCompound();
+                    tile.writeToNBT(nbt);
+                    nbt.removeTag("x");
+                    nbt.removeTag("y");
+                    nbt.removeTag("z");
+                    nbt.removeTag("id");
+                    return !nbt.isEmpty() ? CanPickUpResult.CAN_PICK_UP : CanPickUpResult.NO_DATA;
+                }
+                return CanPickUpResult.BANNED_TILE;
+            }
+            return CanPickUpResult.NO_TILE;
+        }
+        return CanPickUpResult.BANNED_BLOCK;
+    }
 
 }
