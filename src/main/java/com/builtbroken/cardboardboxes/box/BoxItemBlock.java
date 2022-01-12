6582849d42fce555b240f9227e3cc57ce4b05e2e
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-01-12 01:13:56 +0100

    fix #8

--- src/main/java/com/builtbroken/cardboardboxes/box/BoxItemBlock.java
+++ src/main/java/com/builtbroken/cardboardboxes/box/BoxItemBlock.java
@@ -162,7 +162,7 @@ public class BoxItemBlock extends BlockItem {
 
                 //Place audio
                 SoundType soundtype = blockstate.getBlock().getSoundType(blockstate, context.getLevel(), pos, context.getPlayer());
-                context.getLevel().playSound(context.getPlayer(), pos, soundtype.getPlaceSound(), SoundSource.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
+                context.getLevel().playSound(null, pos, soundtype.getPlaceSound(), SoundSource.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
 
                 //Consume item
                 heldItemStack.shrink(1);
