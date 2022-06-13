51ddfc25a3e5e6936f7f2e503ec701c7b23af9d2
bl4ckscor3 <bl4ckscor33@gmail.com>
2022-06-13 18:56:03 +0200

    feature: update to 1.19

--- src/main/java/com/builtbroken/cardboardboxes/box/BoxBlock.java
+++ src/main/java/com/builtbroken/cardboardboxes/box/BoxBlock.java
@@ -6,7 +6,7 @@ import com.builtbroken.cardboardboxes.Cardboardboxes;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -29,90 +29,90 @@ import net.minecraft.world.phys.HitResult;
  * Created by Dark(DarkGuardsman, Robert) on 7/28/2015.
  */
 public class BoxBlock extends BaseEntityBlock {
-    public static final String STORE_ITEM_TAG = "storedItem";
-    public static final String BLOCK_ENTITY_DATA_TAG = "tileData";
+	public static final String STORE_ITEM_TAG = "storedItem";
+	public static final String BLOCK_ENTITY_DATA_TAG = "tileData";
 
-    public BoxBlock() {
-        super(Properties.of(Material.WOOD).strength(2f, 2f));
-    }
+	public BoxBlock() {
+		super(Properties.of(Material.WOOD).strength(2f, 2f));
+	}
 
-    @Override
-    public RenderShape getRenderShape(BlockState state) {
-        return RenderShape.MODEL;
-    }
+	@Override
+	public RenderShape getRenderShape(BlockState state) {
+		return RenderShape.MODEL;
+	}
 
-    @Override
-    public void attack(BlockState state, Level level, BlockPos pos, Player player) {
-        if (!level.isClientSide) {
-            if (level.getBlockEntity(pos) instanceof BoxBlockEntity boxBlockEntity && boxBlockEntity.getStateForPlacement() != null) {
-                if (boxBlockEntity.getStateForPlacement() != null && level.setBlock(pos, boxBlockEntity.getStateForPlacement(), 3)) {
-                    CompoundTag compound = boxBlockEntity.getDataForPlacement();
-                    if (compound != null) {
-                        BlockEntity blockEntity = level.getBlockEntity(pos);
-                        if (blockEntity != null) {
-                            blockEntity.load(compound);
-                        }
-                    }
-                    if (!player.isCreative()) {
-                        ItemStack stack = new ItemStack(this);
-                        if (player.getInventory().add(stack)) {
-                            player.spawnAtLocation(stack, 0F);
-                        }
-                    }
-                }
-            }
-        }
-    }
+	@Override
+	public void attack(BlockState state, Level level, BlockPos pos, Player player) {
+		if (!level.isClientSide) {
+			if (level.getBlockEntity(pos) instanceof BoxBlockEntity boxBlockEntity && boxBlockEntity.getStateForPlacement() != null) {
+				if (boxBlockEntity.getStateForPlacement() != null && level.setBlock(pos, boxBlockEntity.getStateForPlacement(), 3)) {
+					CompoundTag compound = boxBlockEntity.getDataForPlacement();
+					if (compound != null) {
+						BlockEntity blockEntity = level.getBlockEntity(pos);
+						if (blockEntity != null) {
+							blockEntity.load(compound);
+						}
+					}
+					if (!player.isCreative()) {
+						ItemStack stack = new ItemStack(this);
+						if (player.getInventory().add(stack)) {
+							player.spawnAtLocation(stack, 0F);
+						}
+					}
+				}
+			}
+		}
+	}
 
-    @Override
-    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand handIn, BlockHitResult hit) {
-        if (level.isClientSide) {
-            return InteractionResult.PASS;
-        }
-        if (player.isShiftKeyDown()) {
-            ItemStack stack = toItemStack(level, pos);
-            if (stack != null) {
-                if (player.getInventory().add(stack)) {
-                    player.getInventory().setChanged();
-                    level.removeBlock(pos, false);
-                    return InteractionResult.SUCCESS;
-                } else {
-                    player.displayClientMessage(new TranslatableComponent(getDescriptionId() + ".inventoryFull"), true);
-                    return InteractionResult.PASS;
-                }
-            } else {
-                player.displayClientMessage(new TranslatableComponent(getDescriptionId() + ".error.stack.null"), true);
-            }
-        }
-        return InteractionResult.PASS;
-    }
+	@Override
+	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand handIn, BlockHitResult hit) {
+		if (level.isClientSide) {
+			return InteractionResult.PASS;
+		}
+		if (player.isShiftKeyDown()) {
+			ItemStack stack = toItemStack(level, pos);
+			if (stack != null) {
+				if (player.getInventory().add(stack)) {
+					player.getInventory().setChanged();
+					level.removeBlock(pos, false);
+					return InteractionResult.SUCCESS;
+				} else {
+					player.displayClientMessage(Component.translatable(getDescriptionId() + ".inventoryFull"), true);
+					return InteractionResult.PASS;
+				}
+			} else {
+				player.displayClientMessage(Component.translatable(getDescriptionId() + ".error.stack.null"), true);
+			}
+		}
+		return InteractionResult.PASS;
+	}
 
-    public ItemStack toItemStack(BlockGetter level, BlockPos pos) {
-        ItemStack stack = new ItemStack(Cardboardboxes.BOX_BLOCK.get());
+	public ItemStack toItemStack(BlockGetter level, BlockPos pos) {
+		ItemStack stack = new ItemStack(Cardboardboxes.BOX_BLOCK.get());
 
-        if (level.getBlockEntity(pos) instanceof BoxBlockEntity blockEntity) {
-            if (blockEntity.getStateForPlacement() != null) {
-                stack.setTag(new CompoundTag());
+		if (level.getBlockEntity(pos) instanceof BoxBlockEntity blockEntity) {
+			if (blockEntity.getStateForPlacement() != null) {
+				stack.setTag(new CompoundTag());
 
-                stack.getTag().putInt(STORE_ITEM_TAG, Block.getId(blockEntity.getStateForPlacement()));
-                if (blockEntity.getDataForPlacement() != null) {
-                    stack.getTag().put(BLOCK_ENTITY_DATA_TAG, blockEntity.getDataForPlacement());
-                }
-            } else {
-                System.out.println("Error: block entity does not have an ItemStack");
-            }
-        }
-        return stack;
-    }
+				stack.getTag().putInt(STORE_ITEM_TAG, Block.getId(blockEntity.getStateForPlacement()));
+				if (blockEntity.getDataForPlacement() != null) {
+					stack.getTag().put(BLOCK_ENTITY_DATA_TAG, blockEntity.getDataForPlacement());
+				}
+			} else {
+				System.out.println("Error: block entity does not have an ItemStack");
+			}
+		}
+		return stack;
+	}
 
-    @Override
-    public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter level, BlockPos pos, Player player) {
-        return toItemStack(level, pos);
-    }
+	@Override
+	public ItemStack getCloneItemStack(BlockState state, HitResult target, BlockGetter level, BlockPos pos, Player player) {
+		return toItemStack(level, pos);
+	}
 
-    @Nullable
-    @Override
-    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        return new BoxBlockEntity(pos, state);
-    }
+	@Nullable
+	@Override
+	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		return new BoxBlockEntity(pos, state);
+	}
 }
--- src/main/java/com/builtbroken/cardboardboxes/mods/ModHandler.java
+++ src/main/java/com/builtbroken/cardboardboxes/mods/ModHandler.java
@@ -21,112 +21,112 @@ import net.minecraftforge.registries.IForgeRegistry;
  * Created by Dark(DarkGuardsman, Robert) on 7/28/2015.
  */
 public class ModHandler {
-    public static HashMap<String, Class<? extends ModHandler>> modSupportHandlerMap = new HashMap<>();
-    public static HashMap<String, ModHandler> modSupportHandlerMap_instances = new HashMap<>();
+	public static HashMap<String, Class<? extends ModHandler>> modSupportHandlerMap = new HashMap<>();
+	public static HashMap<String, ModHandler> modSupportHandlerMap_instances = new HashMap<>();
 
-    protected static IForgeRegistry<BlockEntityType<?>> BLOCK_ENTITIES_REGISTRY;
-    public static HashMap<String, ForgeConfigSpec.BooleanValue> blockEntityBanConfigMap = new HashMap<>();
+	protected static IForgeRegistry<BlockEntityType<?>> BLOCK_ENTITIES_REGISTRY;
+	public static HashMap<String, ForgeConfigSpec.BooleanValue> blockEntityBanConfigMap = new HashMap<>();
 
-    public void load(ForgeConfigSpec configuration) {
-    }
+	public void load(ForgeConfigSpec configuration) {
+	}
 
-    public void build(ForgeConfigSpec.Builder b) {
-    }
+	public void build(ForgeConfigSpec.Builder b) {
+	}
 
-    /**
-     * Called to build config
-     */
-    public static ForgeConfigSpec buildHandlerData() {
-        //Create the builder
-        ForgeConfigSpec.Builder b = new ForgeConfigSpec.Builder();
-        LOGGER.info("ModHandler#buildHandlerData() -> Loading data and data handlers");
-        loadBlockEntityRegistry();
-        buildHandlers(b);
-        buildConfig(b);
-        return b.build();
-    }
+	/**
+	 * Called to build config
+	 */
+	public static ForgeConfigSpec buildHandlerData() {
+		//Create the builder
+		ForgeConfigSpec.Builder b = new ForgeConfigSpec.Builder();
+		LOGGER.info("ModHandler#buildHandlerData() -> Loading data and data handlers");
+		loadBlockEntityRegistry();
+		buildHandlers(b);
+		buildConfig(b);
+		return b.build();
+	}
 
-    /**
-     * Called to process handlers
-     *
-     * @param configuration
-     */
-    public static void loadHandlerData(ForgeConfigSpec configuration) {
-        LOGGER.info("ModHandler#loadHandlerData() -> Accessed Block Entity Registry: " + (BLOCK_ENTITIES_REGISTRY != null));
-        processHandlers(configuration);
-        LOGGER.info("ModHandler#loadHandlerData() -> Finished loading data handlers");
-        loadConfig(configuration);
-        LOGGER.info("ModHandler#loadHandlerData() -> Finished loading configurations");
-    }
+	/**
+	 * Called to process handlers
+	 *
+	 * @param configuration
+	 */
+	public static void loadHandlerData(ForgeConfigSpec configuration) {
+		LOGGER.info("ModHandler#loadHandlerData() -> Accessed Block Entity Registry: " + (BLOCK_ENTITIES_REGISTRY != null));
+		processHandlers(configuration);
+		LOGGER.info("ModHandler#loadHandlerData() -> Finished loading data handlers");
+		loadConfig(configuration);
+		LOGGER.info("ModHandler#loadHandlerData() -> Finished loading configurations");
+	}
 
-    private static void buildHandlers(ForgeConfigSpec.Builder b) {
-        for (Map.Entry<String, Class<? extends ModHandler>> entry : ModHandler.modSupportHandlerMap.entrySet()) {
-            if (ModList.get().isLoaded(entry.getKey()) || entry.getKey().equals("minecraft")) {
-                try {
-                    ModHandler instance = entry.getValue().newInstance();
-                    modSupportHandlerMap_instances.put(entry.getKey(), instance);
-                    instance.build(b);
-                } catch (InstantiationException e) {
-                    LOGGER.error("Failed to create handler for mod " + entry.getKey());
-                    e.printStackTrace();
-                } catch (IllegalAccessException e) {
-                    LOGGER.error("Failed to access constructor for handler for mod " + entry.getKey());
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+	private static void buildHandlers(ForgeConfigSpec.Builder b) {
+		for (Map.Entry<String, Class<? extends ModHandler>> entry : ModHandler.modSupportHandlerMap.entrySet()) {
+			if (ModList.get().isLoaded(entry.getKey()) || entry.getKey().equals("minecraft")) {
+				try {
+					ModHandler instance = entry.getValue().newInstance();
+					modSupportHandlerMap_instances.put(entry.getKey(), instance);
+					instance.build(b);
+				} catch (InstantiationException e) {
+					LOGGER.error("Failed to create handler for mod " + entry.getKey());
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					LOGGER.error("Failed to access constructor for handler for mod " + entry.getKey());
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    private static void processHandlers(ForgeConfigSpec configuration) {
-        for (Map.Entry<String, ModHandler> entry : ModHandler.modSupportHandlerMap_instances.entrySet()) {
-            entry.getValue().load(configuration);
-        }
-    }
+	private static void processHandlers(ForgeConfigSpec configuration) {
+		for (Map.Entry<String, ModHandler> entry : ModHandler.modSupportHandlerMap_instances.entrySet()) {
+			entry.getValue().load(configuration);
+		}
+	}
 
-    public static void buildConfig(ForgeConfigSpec.Builder b) {
-        String comment = "Auto generated list of block entities registered in Minecraft that can be blocked from use with the box. " +
-                "If a block entity does not show up on this list it is already black listed. The reasoning behind blocking block entities is to prevent crashes or unwanted " +
-                "interaction. Such as picking up a piston which can both causes issues and doesn't really matter. Set value to 'true' to disable interaction.";
-        b.comment(comment).push("tile_ban_list"); //set the category
-        for (ResourceLocation name : BLOCK_ENTITIES_REGISTRY.getKeys()) {
-            BlockEntityType<?> type = BLOCK_ENTITIES_REGISTRY.getValue(name);
-            if (name != null && type != null) {
-                try {
-                    String typeString = type.getRegistryName().toString();
-                    boolean shouldBan = HandlerManager.blockEntityBanList.contains(type) || typeString.contains("cable") || typeString.contains("wire") || typeString.contains("pipe") || typeString.contains("tube") || typeString.contains("conduit") || typeString.contains("channel");
-                    blockEntityBanConfigMap.put(typeString, b.define(typeString, shouldBan));
-                } catch (Exception e) {
-                    LOGGER.error("ModHandler#buildConfig() -> Failed to add entry to config [" + name + " > " + type + "]", e);
-                }
-            }
-        }
-        b.pop(); //go back to top level category
+	public static void buildConfig(ForgeConfigSpec.Builder b) {
+		String comment = "Auto generated list of block entities registered in Minecraft that can be blocked from use with the box. " +
+				"If a block entity does not show up on this list it is already black listed. The reasoning behind blocking block entities is to prevent crashes or unwanted " +
+				"interaction. Such as picking up a piston which can both causes issues and doesn't really matter. Set value to 'true' to disable interaction.";
+		b.comment(comment).push("tile_ban_list"); //set the category
+		for (ResourceLocation name : BLOCK_ENTITIES_REGISTRY.getKeys()) {
+			BlockEntityType<?> type = BLOCK_ENTITIES_REGISTRY.getValue(name);
+			if (name != null && type != null) {
+				try {
+					String typeString = ForgeRegistries.BLOCK_ENTITIES.getKey(type).toString();
+					boolean shouldBan = HandlerManager.blockEntityBanList.contains(type) || typeString.contains("cable") || typeString.contains("wire") || typeString.contains("pipe") || typeString.contains("tube") || typeString.contains("conduit") || typeString.contains("channel");
+					blockEntityBanConfigMap.put(typeString, b.define(typeString, shouldBan));
+				} catch (Exception e) {
+					LOGGER.error("ModHandler#buildConfig() -> Failed to add entry to config [" + name + " > " + type + "]", e);
+				}
+			}
+		}
+		b.pop(); //go back to top level category
 
-    }
+	}
 
-    private static void loadConfig(ForgeConfigSpec configuration) {
-        if (BLOCK_ENTITIES_REGISTRY != null) {
-            for (ResourceLocation name : BLOCK_ENTITIES_REGISTRY.getKeys()) {
-                BlockEntityType<?> type = BLOCK_ENTITIES_REGISTRY.getValue(name);
-                if (name != null && type != null) {
-                    try {
-                        String typeString = type.getRegistryName().toString();
-                        boolean shouldBan = HandlerManager.blockEntityBanList.contains(type) || typeString.contains("cable") || typeString.contains("wire") || typeString.contains("pipe") || typeString.contains("tube") || typeString.contains("conduit") || typeString.contains("channel");
-                        if (blockEntityBanConfigMap.containsKey(typeString) ? blockEntityBanConfigMap.get(typeString).get() : false) {
-                            HandlerManager.INSTANCE.banBlockEntity(type);
-                        } else if (shouldBan) {
-                            //If original was banned but someone unbanned it in the config
-                            HandlerManager.blockEntityBanList.remove(type);
-                        }
-                    } catch (Exception e) {
-                        LOGGER.error("ModHandler#loadHandlerData() -> Failed to add entry to config [" + name + " > " + type + "]", e);
-                    }
-                }
-            }
-        }
-    }
+	private static void loadConfig(ForgeConfigSpec configuration) {
+		if (BLOCK_ENTITIES_REGISTRY != null) {
+			for (ResourceLocation name : BLOCK_ENTITIES_REGISTRY.getKeys()) {
+				BlockEntityType<?> type = BLOCK_ENTITIES_REGISTRY.getValue(name);
+				if (name != null && type != null) {
+					try {
+						String typeString = ForgeRegistries.BLOCK_ENTITIES.getKey(type).toString();
+						boolean shouldBan = HandlerManager.blockEntityBanList.contains(type) || typeString.contains("cable") || typeString.contains("wire") || typeString.contains("pipe") || typeString.contains("tube") || typeString.contains("conduit") || typeString.contains("channel");
+						if (blockEntityBanConfigMap.containsKey(typeString) ? blockEntityBanConfigMap.get(typeString).get() : false) {
+							HandlerManager.INSTANCE.banBlockEntity(type);
+						} else if (shouldBan) {
+							//If original was banned but someone unbanned it in the config
+							HandlerManager.blockEntityBanList.remove(type);
+						}
+					} catch (Exception e) {
+						LOGGER.error("ModHandler#loadHandlerData() -> Failed to add entry to config [" + name + " > " + type + "]", e);
+					}
+				}
+			}
+		}
+	}
 
-    private static void loadBlockEntityRegistry() {
-        BLOCK_ENTITIES_REGISTRY = ForgeRegistries.BLOCK_ENTITIES;
-    }
+	private static void loadBlockEntityRegistry() {
+		BLOCK_ENTITIES_REGISTRY = ForgeRegistries.BLOCK_ENTITIES;
+	}
 }
